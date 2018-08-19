86b193710ee38afbf28a8154e00e8a16a2295a1e
Alex Sorafumo <alex@yuion.net>
2018-08-19 14:23:54 +1000

    Update build logic

--- config/ng.ts
+++ config/ng.ts
@@ -1,15 +1,19 @@
 import { spawn } from 'child_process';
+import { platform } from 'process';
 
 const proc = (cmd, args) =>
     new Promise((resolve, reject) => {
-        const p = spawn(cmd, args);
+        const p = createSpawn(cmd, args);
         p.stdout.pipe(process.stdout);
         p.stderr.pipe(process.stderr);
-        p.on('exit', (code, signal) =>
-            code == 0 ? resolve() : reject(signal));
+        p.on('exit', (code, signal) => code == 0 ? resolve() : reject(signal));
     });
 
 const npmScript = (name) => (...args) =>
     proc('npm', ['run', name, '--', ...args]);
+
+function createSpawn(cmd, args) {
+    return platform === 'win32' ? spawn(cmd, args, { shell: true }) : spawn(cmd, args);
+}
 
 export const ng = npmScript('ng');
