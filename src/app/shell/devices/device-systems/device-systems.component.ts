af8504a8965812bdd2bc3f5d7e2f73612b13cce0
Alex Sorafumo <alex@yuion.net>
2018-10-11 00:08:39 +1100

    Update devices item tab component views

--- src/app/shell/devices/device-systems/device-systems.component.ts
+++ src/app/shell/devices/device-systems/device-systems.component.ts
@@ -1,5 +1,5 @@
 
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 
 import { BaseComponent } from '../../../shared/components/base.component';
 import { AppService } from '../../../services/app.service';
@@ -10,7 +10,7 @@ import { IEngineModule } from '../../../services/data/modules.service';
     templateUrl: './device-systems.template.html',
     styleUrls: ['./device-systems.styles.scss']
 })
-export class DeviceSystemsComponent extends BaseComponent {
+export class DeviceSystemsComponent extends BaseComponent implements OnChanges {
     @Input() public item: IEngineModule;
 
     public model: any = {};
@@ -18,10 +18,27 @@ export class DeviceSystemsComponent extends BaseComponent {
     constructor(private service: AppService) {
         super();
     }
-    
+
     public ngOnChanges(changes: any) {
         if (changes.item) {
-            
+            this.load();
+        }
+    }
+
+    public load(offset: number = 0) {
+        this.service.Systems.query({ module_id: this.item.id, offset }).then((list) => {
+            this.model.list = list;
+        }, () => null);
+    }
+
+    public goto(item, link?: string) {
+        if (link) {
+            if (link.indexOf('http://') < 0 && link.indexOf('https://') < 0) {
+                link = `http${item.tls}://${link}${item.port ? ':' + item.port : ''}`;
+            }
+            window.open(item, '_blank');
+        } else {
+            this.service.navigate(['systems', item.id, 'devices']);
         }
     }
 }
