c65f118ca1f95ea02b8afcaf7441c718f8748b10
Alex Sorafumo <alex@yuion.net>
2018-10-19 21:25:10 +1100

    Add query to dropdowns on system modal

--- src/app/overlays/system-modal/system-modal.component.ts
+++ src/app/overlays/system-modal/system-modal.component.ts
@@ -9,8 +9,12 @@ import { OverlayContentComponent } from '@acaprojects/ngx-widgets';
 })
 export class SystemModalComponent extends OverlayContentComponent implements OnInit {
 
+    private timers: any = {};
+
     public ngOnInit() {
         this.model.form = {};
+        this.model.zones = [];
+        this.model.nodes = [];
     }
 
     public init() {
@@ -26,6 +30,9 @@ export class SystemModalComponent extends OverlayContentComponent implements OnI
                 settings: JSON.parse(JSON.stringify(this.model.item.settings || {})),
                 edge_id: this.model.item.edge_id
             };
+        } else {
+            this.loadZones();
+            this.loadNodes();
         }
     }
 
@@ -35,6 +42,29 @@ export class SystemModalComponent extends OverlayContentComponent implements OnI
 
     public edit() {
 
+    }
+
+    public load(query: string = '', type: string = 'Zones') {
+        if (this.model[`loading_${type}`]) {
+            if (this.timers[`${type}`]) { clearTimeout(this.timers[`${type}`]); }
+            return this.timers[`${type}`] = setTimeout(() => {
+                this.timers[`${type}`] = null;
+                this.loadZones();
+            }, 300);
+        }
+        this.model[`loading_${type}`] = true;
+        this.service[`${type}`].query({ q: query, offset: '0' }).then((list) => {
+            this.model[`${(type || '').toLowerCase()}`] = list;
+            this.model[`loading_${type}`] = false;
+        }, () => this.model[`loading_${type}`] = false);
+    }
+
+    public loadZones(query: string = '') {
+        this.load(query, 'Zones');
+    }
+
+    public loadNodes(query: string = '') {
+        // this.load(query, 'Nodes');
     }
 
 }
