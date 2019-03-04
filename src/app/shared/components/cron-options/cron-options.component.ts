55ed40fd9e7dbd51f57430d72b016f8c56b99c04
Alex Sorafumo <alex@yuion.net>
2019-03-04 15:32:26 +1100

    Add component for selecting cron settings

---
+++ src/app/shared/components/cron-options/cron-options.component.ts
@@ -0,0 +1,31 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+import { BaseComponent } from '../base.component';
+
+import * as moment from 'moment';
+
+const CRON_PERIOD: any[] = [
+    { id: 'minute', name: 'Minutely', shorthand: 'm' },
+    { id: 'hourly', name: 'Hourly', shorthand: 'H' },
+    { id: 'daily', name: 'Daily', shorthand: 'D' },
+    { id: 'weekdays', name: 'Weekdays', shorthand: 'WD' },
+    { id: 'weekend', name: 'Weekends', shorthand: 'WE' },
+    { id: 'weekly', name: 'Weekly', shorthand: 'WK' },
+    { id: 'monthly', name: 'Monthly', shorthand: 'M' },
+    { id: 'monthly', name: 'Yearly', shorthand: 'Y' }
+];
+
+@Component({
+    selector: 'cron',
+    templateUrl: './cron-options.template.html',
+    styleUrls: ['./cron-options.styles.scss']
+})
+export class CronOptionsComponent extends BaseComponent implements OnInit {
+    @Input() public cron: string;
+
+    public model: any = {};
+
+    public ngOnInit() {
+        this.model.periods = CRON_PERIOD;
+    }
+}
