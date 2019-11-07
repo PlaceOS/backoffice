5ce806dcda86bb2af4b54196bf1cf531e5e7213d
Alex Sorafumo <alex@yuion.net>
2019-11-08 10:55:23 +1100

    chore(settings): integrate settings with the monaco editor

--- src/app/shared/components/settings-display/settings-display.component.ts
+++ src/app/shared/components/settings-display/settings-display.component.ts
@@ -1,19 +1,18 @@
-
-import { Component, Input, OnChanges, EventEmitter, Output, Renderer2, ViewChild, ElementRef } from '@angular/core';
+/// <reference path="../../../../../node_modules/monaco-editor/monaco.d.ts" />
+import { Component, Input, OnChanges, EventEmitter, Output, Renderer2, ViewChild, ElementRef, OnInit, SimpleChanges } from '@angular/core';
 
 import { ApplicationService } from '../../../services/app.service';
 import { BaseDirective } from '../../globals/base.directive';
-
-import * as json_lint from 'durable-json-lint';
 import { copyToClipboard } from '../../utilities/general.utilities';
+import { HashMap } from '../../utilities/types.utilities';
 
 @Component({
     selector: 'settings-display',
     templateUrl: './settings-display.template.html',
     styleUrls: ['./settings-display.styles.scss']
 })
-export class SettingsDisplayComponent extends BaseDirective implements OnChanges {
-    @Input() public model: any;
+export class SettingsDisplayComponent extends BaseDirective implements OnInit {
+    @Input() public model: HashMap;
     @Input() public group = '';
     @Input() public show = true;
     @Input() public sub = false;
@@ -21,168 +20,38 @@ export class SettingsDisplayComponent extends BaseDirective implements OnChanges
     @Output() public valid = new EventEmitter();
     @Output() public modelChange = new EventEmitter();
 
-    public settings = [];
+    @ViewChild('editor', { static: true }) private element: ElementRef;
 
-    public length = 0;
-    public hover = -1;
-    public depth = 0;
-    public focused = false;
-    public text_string = '{}';
-    public error_list: any = {};
+    private editor: any;
 
-    @ViewChild('input', { static: true }) public input_field: ElementRef;
+    public get code(): string {
+        return typeof this.model === 'object' ? JSON.stringify(this.model, null, 4) : `${this.model}` || ''
+    }
 
     constructor(private service: ApplicationService, private renderer: Renderer2) {
         super();
     }
 
-    public ngOnChanges(changes: any) {
-        if (changes.model) {
-            this.timeout('change', () => {
-                if (!this.model) { this.model = {}; }
-                if (this.input && this.text_string === '{}') {
-                    this.text_string = JSON.stringify(this.model || '{}', null, 4);
-                    this.updateValue();
-                }
-                this.updateSettings();
-            }, 100);
+    public ngOnInit(): void {
+        if (this.element && this.element.nativeElement) {
+            this.editor = monaco.editor.create(this.element.nativeElement, {
+                value: this.code,
+                language: 'json',
+                fontFamily: 'Fira Code',
+
+                lineNumbers: 'on',
+                roundedSelection: false,
+                scrollBeyondLastLine: false,
+                readOnly: !this.input,
+                theme: 'vs-dark'
+            });
         }
     }
 
-    public updateSettings() {
-        if (this.input) { return this.inputToSettings(); }
-        this.length = 0;
-        this.settings = [];
-        let formatted = JSON.stringify(this.model, null, 4);
-        if (formatted === '{}') {
-            formatted = (this.text_string || '{}');
+    public ngOnChanges(changes: SimpleChanges): void {
+        if (changes.model && this.model && this.editor) {
+            this.editor.setValue(this.code);
         }
-        const lines = formatted.split('\n');
-        while (lines.length < 4) { lines.push(''); }
-        for (const line of lines) {
-            const line_data: any = {
-                index: lines.indexOf(line),
-                value: line,
-                depth: (line.match(/ {4}/g) || []).length - 1,
-                formatted: this.format(line)
-            };
-            if (this.error_list) {
-                for (const error of (this.error_list.errors || [])) {
-                    if (line_data.index + 1 === error.lineNumber) {
-                        line_data.error = error.description;
-                        break;
-                    }
-                }
-            }
-            this.settings.push(line_data);
-            this.length = line.length > this.length ? line.length : this.length;
-        }
-    }
-
-    public hovering(i) {
-        this.hover = i;
-        this.depth = this.settings[i].depth || 0;
-    }
-
-    public leave(i) {
-        this.hover = -1;
-        this.depth = 0;
-    }
-
-    public focus() {
-        this.focused = true;
-        this.updateCursor();
-    }
-
-    public updateCursor() {
-
-    }
-
-    public change(e) {
-        this.text_string = e.target.innerHTML
-            .replace(/\<div[ \/a-zA-Z0-9=\";:\.-]*\>/g, '\n')
-            .replace(/\<[ \/a-zA-Z0-9=\";:\.-]*\>/g, '');
-        this.checkSettings();
-    }
-
-    public updateValue() {
-        if (!this.input_field) {
-            return this.timeout('update', () => this.updateValue());
-        }
-        const div = this.input_field.nativeElement;
-        const value = this.text_string.replace(/\n/g, '</div><div>').replace('</div>', '');
-        this.renderer.setProperty(div, 'innerHTML', value);
-    }
-
-    public checkSettings() {
-        this.error_list = json_lint(this.text_string);
-        if (this.error_list && this.error_list.errors.length <= 0) {
-            try {
-                this.model = JSON.parse(this.text_string);
-                this.modelChange.emit(this.model);
-                this.valid.emit(true);
-            } catch (e) {
-                const message: string = e.message;
-                const pos = this.getPosition(+(message.match(/[0-9]*$/g)[0]));
-                this.error_list.errors.push({ description: message, lineNumber: pos.line, column: pos.column });
-                this.valid.emit(false);
-            }
-        } else {
-            this.valid.emit(false);
-        }
-        this.inputToSettings();
-    }
-
-    public inputToSettings() {
-        this.length = 0;
-        this.settings = [];
-        const lines = (this.text_string || '{}').split('\n');
-        while (lines.length < 4) { lines.push(''); }
-        for (const line of lines) {
-            const line_data: any = {
-                index: lines.indexOf(line),
-                value: line,
-                depth: (line.match(/ {4}/g) || []).length - 1,
-                formatted: this.format(line)
-            };
-            if (this.error_list) {
-                for (const error of (this.error_list.errors || [])) {
-                    if (line_data.index + 1 === error.lineNumber) {
-                        line_data.error = error.description || error.message;
-                        break;
-                    }
-                }
-            }
-            this.settings.push(line_data);
-            this.length = line.length > this.length ? line.length : this.length;
-        }
-    }
-
-    public getPosition(position: number): { line: number, column: number } {
-        const lines = (this.text_string || '').split('\n');
-        let remainder = position;
-        for (const line of lines) {
-            if (line.length + 1 < remainder) {
-                remainder -= line.length + 1;
-            } else {
-                return { line: lines.indexOf(line) + 1, column: remainder };
-            }
-        }
-        return { line: 1, column: position };
-    }
-
-    public format(line) {
-        return line.replace(/"[^:]*"/g, '<span class="string">$&</span>')
-            .replace(/\<span class="string"\>".*"\<\/span\> *:/g, '<span class="key">$&</span>')
-            .replace(/[0-9]*.?[0-9]+ *,?/g, '<span class="number">$&</span>')
-            .replace(/(true|false) *,?/g, '<span class="boolean">$&</span>')
-            .replace(/(null|nil|undefined) *,?/g, '<span class="null">$&</span>')
-            .replace(/ {4}/g, '<div class="depth"><div class="bar"></div>&nbsp;</div>&nbsp;&nbsp;&nbsp;')
-                // Prevent separators from being colours
-            .replace(/:\<\/span\>/g, '</span>:')
-            .replace(/,\<\/span\>/g, '</span>,')
-                // Add hyperlinks to URLs
-            .replace(/^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:\/?#[\]@!\$&'\(\)\*\+,;=.]+$/g, '<a href="$&">$&</a>');
     }
 
     public copy() {
