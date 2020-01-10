c06b3175a8d77f8ebe968019796bffbee1258c42
Alex Sorafumo <alex@yuion.net>
2020-01-10 12:09:36 +1100

    chore(devices): fix device systems component

--- src/app/shell/devices/device-systems/device-systems.component.ts
+++ src/app/shell/devices/device-systems/device-systems.component.ts
@@ -1,9 +1,11 @@
 
 import { Component, Input, OnChanges } from '@angular/core';
-import { EngineModule } from '@acaprojects/ts-composer';
+import { EngineModule, EngineSystem } from '@acaprojects/ts-composer';
 
 import { BaseDirective } from '../../../shared/globals/base.directive';
 import { ApplicationService } from '../../../services/app.service';
+import { Subject, Observable, of } from 'rxjs';
+import { debounceTime, distinctUntilChanged, switchMap, catchError, map } from 'rxjs/operators';
 
 @Component({
     selector: 'device-systems',
@@ -12,33 +14,52 @@ import { ApplicationService } from '../../../services/app.service';
 })
 export class DeviceSystemsComponent extends BaseDirective implements OnChanges {
     @Input() public item: EngineModule;
-
-    public model: any = {};
+    /** Filter string for the system list */
+    public search_str: string;
+    /** List of systems associated with the module */
+    public system_list: EngineSystem[]
+    /** List of items from an API search */
+    public search_results$: Observable<EngineSystem[]>;
+    /** Subject holding the value of the search */
+    public search$ = new Subject<string>();
+    /** Whether systems are being loaded */
+    public loading: boolean;
 
     constructor(private service: ApplicationService) {
         super();
     }
 
+    public ngOnInit() {
+        this.search_results$ = this.search$.pipe(
+            debounceTime(400),
+            distinctUntilChanged(),
+            switchMap(query => {
+                this.loading = true;
+                return this.service.Systems.query({ q: query, module_id: this.item.id, offset: 0 })
+            }),
+            catchError((err) => {
+                console.error(err);
+                return of([]);
+            }),
+            map((list: EngineSystem[]) => {
+                this.loading = false;
+                const search = this.search_str.toLowerCase();
+                return list.filter((item: any) => item.name.toLowerCase().indexOf(search) >= 0 || (item.email || '').toLowerCase().indexOf(search) >= 0);
+            })
+        );
+        // Process API results
+        this.subscription('search_results', this.search_results$.subscribe(list => this.system_list = list));
+    }
+
     public ngOnChanges(changes: any) {
         if (changes.item) {
-            this.load();
+            this.loadSystems();
         }
     }
 
-    public load(offset: number = 0) {
+    public loadSystems(offset: number = 0) {
         this.service.Systems.query({ module_id: this.item.id, offset }).then((list) => {
-            this.model.list = list;
+            this.system_list = list;
         }, () => null);
-    }
-
-    public goto(item, link?: string) {
-        if (link) {
-            if (link.indexOf('http://') < 0 && link.indexOf('https://') < 0) {
-                link = `http${item.tls}://${link}${item.port ? ':' + item.port : ''}`;
-            }
-            window.open(item, '_blank');
-        } else {
-            this.service.navigate(['systems', item.id, 'devices']);
-        }
     }
 }
