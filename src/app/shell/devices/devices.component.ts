975ba2d5924bd5afaba7aa299af18d3fd79c6826
Alex Sorafumo <alex@yuion.net>
2020-01-10 12:22:45 +1100

    chore(devices): hook up edit and delete for devices

--- src/app/shell/devices/devices.component.ts
+++ src/app/shell/devices/devices.component.ts
@@ -6,6 +6,8 @@ import { EngineModule } from '@acaprojects/ts-composer';
 import { ApplicationService } from '../../services/app.service';
 import { BaseRootComponent } from '../../shared/components/base-root.component';
 import { ItemCreateUpdateModalComponent } from 'src/app/overlays/item-modal/item-modal.component';
+import { ConfirmModalComponent, ConfirmModalData } from 'src/app/overlays/confirm-modal/confirm-modal.component';
+import { DialogEvent } from 'src/app/shared/utilities/types.utilities';
 
 @Component({
     selector: 'app-devices',
@@ -48,5 +50,54 @@ export class DevicesComponent extends BaseRootComponent<EngineModule> {
                 service: this._service.Modules
             }
         });
+    }
+
+    /**
+     * Open the modal to create a new system
+     */
+    protected edit() {
+        if (this.item) {
+            const ref = this._dialog.open(ItemCreateUpdateModalComponent, {
+                height: 'auto',
+                width: 'auto',
+                maxHeight: 'calc(100vh - 2em)',
+                maxWidth: 'calc(100vw - 2em)',
+                data: {
+                    item: this.item,
+                    service: this._service.Systems
+                }
+            });
+        }
+    }
+
+    protected delete() {
+        if (this.item) {
+            const ref = this._dialog.open<ConfirmModalComponent, ConfirmModalData>(ConfirmModalComponent, {
+                height: 'auto',
+                width: '24em',
+                maxHeight: 'calc(100vh - 2em)',
+                maxWidth: 'calc(100vw - 2em)',
+                data: {
+                    title: `Delete device`,
+                    content: `<p>Are you sure you want delete this deice?</p><p>Deleting this will <strong>immediately</strong> remove this device from any system it is associated with</p>`,
+                    icon: { type: 'icon', class: 'backoffice-trash' }
+                }
+            });
+            this.subscription('delete_confirm', ref.componentInstance.event.subscribe((event: DialogEvent) => {
+                console.log('Here', event);
+                if (event.reason === 'done') {
+                    ref.componentInstance.loading = 'Deleting device...';
+                    this.item.delete().then(() => {
+                        this._service.notifySuccess(`Successfully deleted device "${this.item.name}".`);
+                        this._router.navigate(['/modules']);
+                        ref.close();
+                        this.unsub('delete_confirm');
+                    }, (err) => {
+                        ref.componentInstance.loading = null;
+                        this._service.notifyError(`Error deleting device. Error: ${err}`);
+                    });
+                }
+            }));
+        }
     }
 }
