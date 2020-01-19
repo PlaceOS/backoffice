c3c8f22b3e140285ba18d366cd816868203952b3
Alex Sorafumo <alex@yuion.net>
2020-01-20 09:53:15 +1100

    chore(lint): update lint rules

---
+++ src/app/shared/components/forms/trigger-condition-form/trigger-condition-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-trigger-condition-form',
+  templateUrl: './trigger-condition-form.component.html',
+  styleUrls: ['./trigger-condition-form.component.scss']
+})
+export class TriggerConditionFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
