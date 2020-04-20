41932652be6f743519dfcf3ceeb7659a53f1403e
Alex Sorafumo <alex@yuion.net>
2020-04-20 17:24:51 +1000

    chore(drivers): show date instead of from if commits older than a month
    Fixes #39

--- src/app/shared/components/forms/driver-form/driver-form.component.ts
+++ src/app/shared/components/forms/driver-form/driver-form.component.ts
@@ -4,7 +4,7 @@ import {
     EngineRepositoryCommit,
     EngineRepository,
     EngineRepositoriesService,
-    EngineDriverRole
+    EngineDriverRole,
 } from '@placeos/ts-client';
 
 import { BaseDirective } from 'src/app/shared/globals/base.directive';
@@ -21,7 +21,7 @@ dayjs.extend(relativeTime);
 @Component({
     selector: 'driver-form',
     templateUrl: './driver-form.component.html',
-    styleUrls: ['./driver-form.component.scss']
+    styleUrls: ['./driver-form.component.scss'],
 })
 export class DriverFormComponent extends BaseDirective implements OnChanges {
     /** Group of form fields used for creating the system */
@@ -32,7 +32,7 @@ export class DriverFormComponent extends BaseDirective implements OnChanges {
         { id: EngineDriverRole.Device, name: 'Device' },
         { id: EngineDriverRole.Service, name: 'Service' },
         { id: EngineDriverRole.Websocket, name: 'Websocket' },
-        { id: EngineDriverRole.Logic, name: 'Logic' }
+        { id: EngineDriverRole.Logic, name: 'Logic' },
     ];
 
     /** Driver used as a template for the new driver being created */
@@ -77,54 +77,67 @@ export class DriverFormComponent extends BaseDirective implements OnChanges {
         this.driver_list$ = this.repo$.pipe(
             debounceTime(100),
             distinctUntilChanged(),
-            switchMap(repo_id => {
+            switchMap((repo_id) => {
                 this.loading_drivers = true;
                 this.driver_list = [];
-                return this._service.Repositories.listDrivers(repo_id)
+                return this._service.Repositories.listDrivers(repo_id);
             }),
-            catchError(_ => {
+            catchError((_) => {
                 this._service.notifyError(`Error loading driver list. Error: ${_.message || _}`);
-                return of([])
+                return of([]);
             }),
             map((list: any[]) => {
                 this.loading_drivers = false;
                 this.commit_list = [];
-                return (list || []).map(driver => ({
+                return (list || []).map((driver) => ({
                     id: driver,
-                    name: driver.replace(/\//g, ' > ')
+                    name: driver.replace(/\//g, ' > '),
                 }));
             })
         );
-        this.subscription('driver_list', this.driver_list$.subscribe((list) => this.driver_list = list));
+        this.subscription(
+            'driver_list',
+            this.driver_list$.subscribe((list) => (this.driver_list = list))
+        );
         this.commit_list$ = this.driver$.pipe(
             debounceTime(100),
             distinctUntilChanged(),
-            switchMap(driver_id => {
+            switchMap((driver_id) => {
                 this.loading_commits = true;
                 this.commit_list = [];
                 return this._service.Repositories.listCommits(this.base_repo.id, {
-                    driver: `${driver_id}`
-                })
+                    driver: `${driver_id}`,
+                });
             }),
-            catchError(_ => {
-                this._service.notifyError(`Error loading driver's commit list. Error: ${_.message || _}`);
-                return of([])
+            catchError((_) => {
+                this._service.notifyError(
+                    `Error loading driver's commit list. Error: ${_.message || _}`
+                );
+                return of([]);
             }),
             map((list: any[]) => {
                 this.loading_commits = false;
                 if (this.form.controls.commit) {
                     this.base_commit = this.commit_list.find(
-                        commit => commit.id === this.form.controls.commit.value
+                        (commit) => commit.id === this.form.controls.commit.value
                     ) as any;
                 }
-                return (list || []).map((commit: EngineRepositoryCommit) => ({
-                    id: commit.commit,
-                    name: `${commit.subject}`,
-                    extra: dayjs(commit.date).fromNow()
-                }));
+                return (list || []).map((commit: EngineRepositoryCommit) => {
+                    const date = dayjs(commit.date);
+                    return {
+                        id: commit.commit,
+                        name: `${commit.subject}`,
+                        extra: date.isAfter(dayjs().subtract(1, 'm'))
+                            ? date.fromNow()
+                            : date.format('DD MMM YYYY'),
+                    };
+                });
             })
         );
-        this.subscription('commit_list', this.commit_list$.subscribe((list) => this.commit_list = list));
+        this.subscription(
+            'commit_list',
+            this.commit_list$.subscribe((list) => (this.commit_list = list))
+        );
     }
 
     public ngOnChanges(changes: SimpleChanges): void {
@@ -152,7 +165,7 @@ export class DriverFormComponent extends BaseDirective implements OnChanges {
     public updateCommitList(driver: Identity) {
         this.form.controls.file_name.setValue(driver.id);
         this.base_driver = driver;
-        const promise = this.commit_list$.toPromise()
+        const promise = this.commit_list$.toPromise();
         this.driver$.next(`${driver.id}`);
         return promise;
     }
@@ -167,22 +180,24 @@ export class DriverFormComponent extends BaseDirective implements OnChanges {
         this.loading = true;
         this._service.Repositories.driverDetails(this.base_repo.id, {
             driver: `${this.base_driver.id}`,
-            commit: `${event.id}`
+            commit: `${event.id}`,
         }).then(
-            driver => {
+            (driver) => {
                 this.loading = false;
                 if (!this.form.controls.id.value) {
                     this.form.controls.name.setValue(driver.descriptive_name || '');
                     this.form.controls.module_name.setValue(driver.generic_name || '');
                     this.form.controls.class_name.setValue(this.base_driver.id || '');
-                    this.form.controls.default_port.setValue(driver.tcp_port || driver.udp_port || '');
+                    this.form.controls.default_port.setValue(
+                        driver.tcp_port || driver.udp_port || ''
+                    );
                     this.form.controls.default_uri.setValue(driver.uri_base || '');
                     this.form.controls.role.setValue(
                         driver.tcp_port || driver.udp_port
                             ? EngineDriverRole.Device
                             : driver.uri_base
-                                ? EngineDriverRole.Service
-                                : EngineDriverRole.Logic
+                            ? EngineDriverRole.Service
+                            : EngineDriverRole.Logic
                     );
                     this.form.controls.settings.setValue(driver.default_settings || '');
                     this.form.controls.description.setValue(driver.description || '');
