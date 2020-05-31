a3c574cc6ab3fcdef86fa6dba3cd3bb27b6f1fbc
Alex Sorafumo <alex@yuion.net>
2020-05-31 15:48:15 +1000

    chore(tests): setup tests to run through jest

--- src/app/overlays/auth-source-modal/auth-source-modal.component.spec.ts
+++ src/app/overlays/auth-source-modal/auth-source-modal.component.spec.ts
@@ -3,23 +3,22 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { AuthSourceModalComponent } from './auth-source-modal.component';
 
 describe('AuthSourceModalComponent', () => {
-  let component: AuthSourceModalComponent;
-  let fixture: ComponentFixture<AuthSourceModalComponent>;
+    let component: AuthSourceModalComponent;
+    let fixture: ComponentFixture<AuthSourceModalComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ AuthSourceModalComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [AuthSourceModalComponent],
+        }).compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(AuthSourceModalComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(AuthSourceModalComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
