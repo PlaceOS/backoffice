1b6c68f12d8bf1cc5792149086117ac96d02937a
Alexandre Chuvand <alexandre@acaprojects.com>
2020-06-26 15:09:14 +1000

    fix(engine): null state #80

--- src/app/shell/engine/details/details.component.ts
+++ src/app/shell/engine/details/details.component.ts
@@ -44,7 +44,7 @@ export class EngineDetailsComponent extends BaseDirective implements OnInit {
     }
 
     public get backoffice_version() {
-        return VERSION.semver.version;
+        return VERSION.semver ? VERSION.semver.version : '';
     }
 
     public get backoffice_build() {
