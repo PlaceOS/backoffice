455cdb40f33e736116c2c972f7cf9eccddcb9918
Alex Sorafumo <alex@yuion.net>
2020-06-30 10:52:55 +1000

    chore(domain): minor logic tweaks

--- src/app/shared/utilities/data/domains.utilities.ts
+++ src/app/shared/utilities/data/domains.utilities.ts
@@ -4,26 +4,24 @@ import { EngineDomain } from '@placeos/ts-client';
 import { FormDetails } from './systems.utilities';
 import { HashMap } from '../types.utilities';
 
-export function generateDomainFormFields(authority: EngineDomain): FormDetails {
-    if (!authority) {
+export function generateDomainFormFields(domain: EngineDomain): FormDetails {
+    if (!domain) {
         throw Error('No User passed to generate form fields');
     }
     const fields: HashMap<FormControl> = {
-        name: new FormControl(authority.name || '', [Validators.required]),
-        domain: new FormControl(authority.domain || '', [Validators.required]),
-        login_url: new FormControl(authority.login_url || ''),
-        logout_url: new FormControl(authority.logout_url || ''),
-        config: new FormControl(authority.config || ''),
-        internals: new FormControl(authority.internals || ''),
-        description: new FormControl(authority.description || ''),
+        name: new FormControl(domain.name || '', [Validators.required]),
+        domain: new FormControl(domain.domain || '', [Validators.required]),
+        login_url: new FormControl(domain.login_url || ''),
+        logout_url: new FormControl(domain.logout_url || ''),
+        config: new FormControl(domain.config || ''),
+        internals: new FormControl(domain.internals || ''),
+        description: new FormControl(domain.description || ''),
     };
     const subscriptions = [];
     for (const key in fields) {
         if (fields[key] && key.indexOf('confirm') < 0) {
             subscriptions.push(
-                fields[key].valueChanges.subscribe(value =>
-                    authority.storePendingChange(key as any, value)
-                )
+                fields[key].valueChanges.subscribe(value => domain.storePendingChange(key as any, value))
             );
         }
     }
