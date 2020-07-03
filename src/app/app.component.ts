8ada97abbd4cbc239530a055bb30dfe084be6608
Alex Sorafumo <alex@yuion.net>
2020-07-03 17:04:11 +1000

    chore(browser): add page for non-supported browsers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,31 @@
 import { Component, ViewEncapsulation, OnInit } from '@angular/core';
+
 import { ApplicationService } from './services/app.service';
+import { BaseDirective } from './shared/globals/base.directive';
+import { detectIE } from './shared/utilities/general.utilities';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
-    styleUrls: ['./styles/app.component.scss', './styles/custom-element.styles.scss', './styles/native-element.styles.scss'],
-    encapsulation: ViewEncapsulation.None
+    styleUrls: [
+        './styles/app.component.scss',
+        './styles/custom-element.styles.scss',
+        './styles/native-element.styles.scss',
+    ],
+    encapsulation: ViewEncapsulation.None,
 })
-export class AppComponent implements OnInit {
-
-    constructor(private _service: ApplicationService) {
-
+export class AppComponent extends BaseDirective implements OnInit {
+    constructor(
+        private _service: ApplicationService,
+    ) {
+        super();
+        /* istanbul ignore if */
+        if (detectIE() && detectIE() < 12) {
+            location.href = `${location.origin}${location.pathname}assets/not-supported.html`;
+            return;
+        }
     }
+
 
     public ngOnInit(): void {
         this._service.title = 'Loading...';
--- src/app/shared/utilities/general.utilities.ts
+++ src/app/shared/utilities/general.utilities.ts
@@ -12,6 +12,37 @@ export function getItemWithKeys(keys: string[], map: HashMap) {
     return null;
 }
 
+/* istanbul ignore next */
+/**
+ * detect IE
+ * returns version of IE or false, if browser is not Internet Explorer
+ */
+export function detectIE() {
+    var ua = window.navigator.userAgent;
+
+    var msie = ua.indexOf('MSIE ');
+    if (msie > 0) {
+        // IE 10 or older => return version number
+        return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+    }
+
+    var trident = ua.indexOf('Trident/');
+    if (trident > 0) {
+        // IE 11 => return version number
+        var rv = ua.indexOf('rv:');
+        return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+    }
+
+    var edge = ua.indexOf('Edge/');
+    if (edge > 0) {
+        // Edge (IE 12+) => return version number
+        return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
+    }
+
+    // other browser
+    return false;
+}
+
 /**
  * Checks whether the platform is a mobile device.
  */
