dac052ea18e7adc91cc97508aaf78245b86d890c
Alex Sorafumo <alex@yuion.net>
2020-08-26 09:28:24 +1000

    fix(auth): update ts-client and fix handling user auth on errors

--- src/app/shared/guards/authorised-admin.guard.ts
+++ src/app/shared/guards/authorised-admin.guard.ts
@@ -9,42 +9,42 @@ import {
     UrlTree,
     Router
 } from '@angular/router';
-import { PlaceUser, currentUser } from '@placeos/ts-client';
+import { PlaceUser } from '@placeos/ts-client';
 import { first } from 'rxjs/operators';
 
 import { ApplicationService } from '../../services/app.service';
+import { BackofficeUsersService } from 'src/app/services/data/users.service';
 
 @Injectable({
     providedIn: 'root'
 })
 export class AuthorisedAdminGuard implements CanActivate, CanLoad {
-
-    private _user: PlaceUser;
-
-    constructor(private _service: ApplicationService, private _router: Router) {}
+    constructor(
+        private _service: ApplicationService,
+        private _router: Router,
+        private _users: BackofficeUsersService
+    ) {}
 
     public async canActivate(
         next: ActivatedRouteSnapshot,
         state: RouterStateSnapshot
     ): Promise<boolean | UrlTree> {
-        await this._service.initialised.pipe(first(_ => _)).toPromise();
-        const user: PlaceUser = this._user || await currentUser().toPromise();
+        await this._service.initialised.pipe(first((_) => _)).toPromise();
+        const user: PlaceUser = await this._users.user.pipe(first((_) => !!_)).toPromise();
         const can_activate = user && user.sys_admin;
         if (!can_activate) {
-            this._router.navigate(['/systems']);
+            this._router.navigate(['/unauthorised']);
         }
-        this._user = user;
         return can_activate;
     }
 
     public async canLoad(route: Route, segments: UrlSegment[]): Promise<boolean> {
-        await this._service.initialised.pipe(first(_ => _)).toPromise();
-        const user: PlaceUser = this._user || await currentUser().toPromise();
+        await this._service.initialised.pipe(first((_) => _)).toPromise();
+        const user: PlaceUser = await this._users.user.pipe(first((_) => !!_)).toPromise();
         const can_activate = user && user.sys_admin;
         if (!can_activate) {
-            this._router.navigate(['/systems']);
+            this._router.navigate(['/unauthorised']);
         }
-        this._user = user;
         return can_activate;
     }
 }
--- src/app/shared/guards/authorised-user.guard.ts
+++ src/app/shared/guards/authorised-user.guard.ts
@@ -10,41 +10,41 @@ import {
     Router,
 } from '@angular/router';
 import { PlaceUser, currentUser } from '@placeos/ts-client';
-import { first } from 'rxjs/operators';
+import { first, delay, retryWhen, take, concatMap } from 'rxjs/operators';
 
 import { ApplicationService } from '../../services/app.service';
+import { BackofficeUsersService } from 'src/app/services/data/users.service';
 
 @Injectable({
     providedIn: 'root',
 })
 export class AuthorisedUserGuard implements CanActivate, CanLoad {
-
-    private _user: PlaceUser;
-
-    constructor(private _service: ApplicationService, private _router: Router) {}
+    constructor(
+        private _service: ApplicationService,
+        private _router: Router,
+        private _users: BackofficeUsersService
+    ) {}
 
     public async canActivate(
         next: ActivatedRouteSnapshot,
         state: RouterStateSnapshot
     ): Promise<boolean | UrlTree> {
-        await this._service.initialised.pipe(first(_ => _)).toPromise();
-        const user: PlaceUser = this._service.get('user') || await currentUser().toPromise();
-        const can_activate = user && user.sys_admin;
+        await this._service.initialised.pipe(first((_) => _)).toPromise();
+        const user: PlaceUser = await this._users.user.pipe(first((_) => !!_)).toPromise();
+        const can_activate = user && (user.sys_admin || user.support);
         if (!can_activate) {
             this._router.navigate(['/unauthorised']);
         }
-        this._user = user;
         return can_activate;
     }
 
     public async canLoad(route: Route, segments: UrlSegment[]): Promise<boolean> {
-        await this._service.initialised.pipe(first(_ => _)).toPromise();
-        const user: PlaceUser = this._user|| await currentUser().toPromise();
-        const can_activate = user && user.sys_admin;
+        await this._service.initialised.pipe(first((_) => _)).toPromise();
+        const user: PlaceUser = await this._users.user.pipe(first((_) => !!_)).toPromise();
+        const can_activate = user && (user.sys_admin || user.support);
         if (!can_activate) {
             this._router.navigate(['/unauthorised']);
         }
-        this._user = user;
         return can_activate;
     }
 }
