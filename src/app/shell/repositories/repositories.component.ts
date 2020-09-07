f66ad4f53e4db026f39f8820c69ebdf840f47896
Alex Sorafumo <alex@yuion.net>
2020-09-08 09:46:48 +1000

    chore(repos): update delete modal text

--- src/app/shell/repositories/repositories.component.ts
+++ src/app/shell/repositories/repositories.component.ts
@@ -135,7 +135,14 @@ export class RepositoriesComponent extends BaseRootComponent<PlaceRepository> {
                     ...CONFIRM_METADATA,
                     data: {
                         title: `Delete repository`,
-                        content: `<p>Are you sure you want delete this repository?</p>`,
+                        content: `
+                            <p>Are you sure you want delete this repository?</p>
+                            ${
+                                this.item.type === PlaceRepositoryType.Driver
+                                    ? '<p>Deleting this repository will <strong>immediately</strong> remove assoicated drivers and modules</p>'
+                                    : ''
+                            }
+                        `,
                         icon: { type: 'icon', class: 'backoffice-trash' },
                     },
                 }
