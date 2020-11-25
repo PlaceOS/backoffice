63e3422e0235f8df183f4f7a740cf20d5ab248d9
Alex Sorafumo <alex@yuion.net>
2020-11-25 13:49:53 +1100

    chore: tweak tailwindcss config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
-module.exports = (isProd) => ({
+
+const colors = require('tailwindcss/colors')
+
+module.exports = (is_build) => ({
     prefix: '',
     important: '#placeos',
     future: {
@@ -6,18 +9,24 @@ module.exports = (isProd) => ({
       purgeLayersByDefault: true
     },
     purge: {
-      enabled: isProd,
+      enabled: is_build && false,
       content: ['**/*.html', '**/*.ts']
     },
     theme: {
-        extend: {
-            colors: {
-                primary: '#C92366',
-                secondary: '#0A0D2E',
-                error: '#e53935',
-                pending: '#ffb300',
-                success: '#43a047'
-            }
+        screens: {
+            sm: '640px'
+        },
+        colors: {
+            black: colors.black,
+            white: colors.white,
+            gray: colors.gray,
+            primary: '#C92366',
+            secondary: '#0A0D2E',
+            ternary: '#0A0D2E',
+            quaternary: '#0A0D2E',
+            error: '#e53935',
+            pending: '#ffb300',
+            success: '#43a047'
         }
     }
 });
