25a70a4e33a618350b881855dea1591b2f447099
Alex Sorafumo <alex@yuion.net>
2020-12-09 12:51:32 +1100

    chore(edge): add button to get token

--- src/app/engine/edge.component.ts
+++ src/app/engine/edge.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
-import { PlaceEdge, queryEdges, removeEdge } from '@placeos/ts-client';
+import { PlaceEdge, queryEdges, removeEdge, retrieveEdgeToken } from '@placeos/ts-client';
 import { BehaviorSubject, Observable, of } from 'rxjs';
 import { catchError, map, shareReplay, switchMap } from 'rxjs/operators';
 
-import { openConfirmModal } from '../common/general';
-import { notifyError, notifySuccess } from '../common/notifications';
+import { copyToClipboard, openConfirmModal } from '../common/general';
+import { notifyError, notifyInfo, notifySuccess } from '../common/notifications';
 import { EdgeModalComponent } from './edge-modal.component';
 
 @Component({
@@ -17,13 +17,16 @@ import { EdgeModalComponent } from './edge-modal.component';
                 <div table-head>
                     <div class="w-32 p-2">Name</div>
                     <div class="flex-1 p-2">description</div>
-                    <div class="w-24 p-2 h-10"></div>
+                    <div class="w-28 p-2 h-10"></div>
                 </div>
                 <div table-body>
                     <div table-row *ngFor="let item of edges | async">
                         <div class="w-32 p-2 truncate">{{ item.name }}</div>
                         <div class="flex-1 p-2 truncate">{{ item.description }}</div>
-                        <div class="w-24 px-2 flex items-center justify-end ">
+                        <div class="w-28 px-2 flex items-center justify-end ">
+                            <button mat-icon-button class="h-10 w-10" (click)="token(item)">
+                                <app-icon className="backoffice-key"></app-icon>
+                            </button>
                             <button mat-icon-button class="h-10 w-10" (click)="edit(item)">
                                 <app-icon className="backoffice-edit"></app-icon>
                             </button>
@@ -74,6 +77,11 @@ export class PlaceEdgeComponent {
         }),
         shareReplay()
     );
+
+    public readonly token = async (edge: PlaceEdge) => {
+        const details = await retrieveEdgeToken(edge.id).toPromise();
+        notifyInfo(`Token: ${details.token}`, 'Copy', () => copyToClipboard(details.token));
+    };
 
     public readonly edit = async (edge?: PlaceEdge) =>
         this._dialog.open(EdgeModalComponent, { data: { edge } });
