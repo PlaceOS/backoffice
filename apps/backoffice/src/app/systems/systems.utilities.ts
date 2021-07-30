0dec226df03a82fd768fae0da154cedfdec71a46
Alex Sorafumo <alex@yuion.net>
2021-07-30 14:59:28 +1000

    fix(systems): update URL validation pattern

--- apps/backoffice/src/app/systems/systems.utilities.ts
+++ apps/backoffice/src/app/systems/systems.utilities.ts
@@ -32,10 +32,10 @@ export const URL_PATTERN =
     '^((http|ftp|ws)s?:\\/\\/)?' + // protocol
     '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
     '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
-    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
-    '(\\?[;&a-zA-Z\\d%_.~+=-]*)?' +
-    '(\\#[-a-zA-Z/\\d_]*)?' +
-    '(\\?[;&a-zA-Z\\d%_.~+=-]*)?$'; // query string;
+    '(\\:\\d+)?(\\/[-a-z{}\\d%_.~+]*)*' + // port and path
+    '(\\?[;&a-zA-Z{}\\d%_.~+=-]*)?' +
+    '(\\#[-a-zA-Z{}/\\d_]*)?' +
+    '(\\?[;&a-zA-Z{}\\d%_.~+=-]*)?$'; // query string;
 
 export function generateSystemsFormFields(system: PlaceSystem): FormGroup {
     if (!system) {
