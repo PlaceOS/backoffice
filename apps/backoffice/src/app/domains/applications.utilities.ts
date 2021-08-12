56e0767e2b0c3cf4e4e43955a91e7442b8ea4ae1
Alex Sorafumo <alex@yuion.net>
2021-08-12 15:49:01 +1000

    fix(domains): add URL validation to application form (#212)

--- apps/backoffice/src/app/domains/applications.utilities.ts
+++ apps/backoffice/src/app/domains/applications.utilities.ts
@@ -3,6 +3,15 @@ import { PlaceApplication } from '@placeos/ts-client';
 
 import { HashMap } from 'apps/backoffice/src/app/common/types';
 
+export const URL_PATTERN =
+    '^((http|ftp|ws)s?:\\/\\/)?' + // protocol
+    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+    '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
+    '(\\:\\d+)?(\\/[-a-z{}\\d%_.~+]*)*' + // port and path
+    '(\\?[;&a-zA-Z{}\\d%_.~+=-]*)?' +
+    '(\\#[-a-zA-Z{}/\\d_]*)?' +
+    '(\\?[;&a-zA-Z{}\\d%_.~+=-]*)?$'; // query string;
+
 export function generateApplicationFormFields(
     app: PlaceApplication
 ): FormGroup {
@@ -13,7 +22,10 @@ export function generateApplicationFormFields(
         name: new FormControl(app.name || '', [Validators.required]),
         scopes: new FormControl(app.scopes || ''),
         skip_authorization: new FormControl(app.skip_authorization || ''),
-        redirect_uri: new FormControl(app.redirect_uri || ''),
+        redirect_uri: new FormControl(
+            app.redirect_uri || '',
+            Validators.pattern(URL_PATTERN)
+        ),
     };
     return new FormGroup(fields);
 }
