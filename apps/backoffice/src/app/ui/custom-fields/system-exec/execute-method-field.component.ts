9b3af93452f4b03ef388147dd729c2196f117021
Alex Sorafumo <alex@yuion.net>
2022-05-03 14:45:13 +1000

    fix(exec): tweak loading overlay for dark mode

--- apps/backoffice/src/app/ui/custom-fields/system-exec/execute-method-field.component.ts
+++ apps/backoffice/src/app/ui/custom-fields/system-exec/execute-method-field.component.ts
@@ -56,7 +56,7 @@ import { ModuleLike } from './select-module.component';
                 </div>
             </div>
             <div
-                class="absolute inset-0 flex flex-col items-center justify-center bg-white bg-opacity-80"
+                class="absolute -inset-2 flex flex-col items-center justify-center bg-white/80 dark:bg-black/40 rounded"
                 *ngIf="loading"
             >
                 <mat-spinner diameter="32"></mat-spinner>
