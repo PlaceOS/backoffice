14261de46d4b957913bfe6f1ed3de2098fdc8dd8
MrYuion <alex@yuion.net>
2023-07-04 06:09:37 +0000

    build(backoffice): CI build from cb2172c

--- assets/monaco/vs/base/common/worker/simpleWorker.nls.fr.js
+++ assets/monaco/vs/base/common/worker/simpleWorker.nls.fr.js
@@ -1,6 +1,6 @@
 /*!-----------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
- * Version: 0.34.1(0316a754aa4c25208bef91937efbce2ab1e3ce37)
+ * Version: 0.39.0(ff3621a3fa6389873be5412d17554294ea1b0941)
  * Released under the MIT license
  * https://github.com/microsoft/vscode/blob/main/LICENSE.txt
  *-----------------------------------------------------------*/define("vs/base/common/worker/simpleWorker.nls.fr",{"vs/base/common/platform":["_"]});
