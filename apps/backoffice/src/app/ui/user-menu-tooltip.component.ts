05c8768ccb43ea1ab1b696749809b9021220b6a3
Alex Sorafumo <alex@yuion.net>
2024-12-10 16:25:05 +1100

    chore(i18n): fix missing translation key

--- apps/backoffice/src/app/ui/user-menu-tooltip.component.ts
+++ apps/backoffice/src/app/ui/user-menu-tooltip.component.ts
@@ -152,7 +152,7 @@ export class UserMenuTooltipComponent {
             { id: 'jp', name: i18n('COMMON.LANG_JAPANESE'), flag: '🇯🇵' },
             { id: 'fr', name: i18n('COMMON.LANG_FRENCH'), flag: '🇫🇷' },
             { id: 'es', name: i18n('COMMON.LANG_SPANISH'), flag: '🇪🇸' },
-            { id: 'ar', name: i18n('COMMON.LANG_ARABIC'), flag: '🇪' },
+            { id: 'ar', name: i18n('COMMON.LANG_ARABIC'), flag: '' },
         ];
     }
 
@@ -164,7 +164,7 @@ export class UserMenuTooltipComponent {
             { id: 'jp', name: i18n('COMMON.LANG_JAPANESE'), flag: '🇯🇵' },
             { id: 'fr', name: i18n('COMMON.LANG_FRENCH'), flag: '🇫🇷' },
             { id: 'es', name: i18n('COMMON.LANG_SPANISH'), flag: '🇪🇸' },
-            { id: 'ar', name: i18n('COMMON.LANG_ARABIC'), flag: '🇪' },
+            { id: 'ar', name: i18n('COMMON.LANG_ARABIC'), flag: '' },
         ];
         setTimeout(() => location.reload(), 100);
     }
