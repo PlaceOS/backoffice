1f3c639d7ffe15e80789bfb8227a27aa15c31549
Alex Sorafumo <alex@yuion.net>
2025-05-29 13:23:26 +1000

    chore(admin): tweak viewing of uploaded media

--- apps/backoffice/src/app/admin/view-upload-modal.component.ts
+++ apps/backoffice/src/app/admin/view-upload-modal.component.ts
@@ -6,16 +6,16 @@ import { UploadInfo } from './upload-library.component';
     selector: 'view-upload-modal',
     template: `
         <header
-            class="flex items-center justify-between border-b border-base-300 p-2"
+            class="sticky top-0 z-10 m-2 w-[calc(100%-1rem)] rounded border border-base-100 bg-base-200 px-4 py-2"
         >
-            <h1 class="px-2">View Upload - {{ name }}</h1>
+            <h2 class="text-xl font-medium">View Upload</h2>
             <button icon matRipple mat-dialog-close>
                 <app-icon>close</app-icon>
             </button>
         </header>
-        <main class="bg-base-200">
+        <main class="px-2 pb-2">
             <div
-                class="flex h-[65vh] max-h-[65vh] w-[80vw] max-w-[80vw] items-center justify-center"
+                class="relative flex h-[65vh] max-h-[65vh] w-[80vw] max-w-[80vw] items-center justify-center overflow-hidden rounded-xl border border-base-200"
             >
                 <img
                     *ngIf="type === 'image'"
@@ -31,11 +31,16 @@ import { UploadInfo } from './upload-library.component';
                     autoplay
                     controls
                 ></video>
+                <div
+                    class="absolute left-2 top-2 max-w-[calc(100%-1rem)] rounded-full border border-base-200 bg-base-100 px-2 py-1 font-mono text-sm"
+                >
+                    {{ name }}
+                </div>
             </div>
         </main>
     `,
     styles: [``],
-    standalone: false
+    standalone: false,
 })
 export class ViewUploadModalComponent {
     public readonly resource = `/api/engine/v2/uploads/${this._data.upload.id}/url`;
--- apps/backoffice/src/app/ui/authenticated-image.directive.ts
+++ apps/backoffice/src/app/ui/authenticated-image.directive.ts
@@ -5,8 +5,8 @@ import { AsyncHandler } from '../common/async-handler.class';
 const IMAGE_STORE = new Map<string, string>();
 
 @Directive({
-    selector: 'img [auth]',
-    standalone: false
+    selector: 'img [auth],video [auth]',
+    standalone: false,
 })
 export class AuthenticatedImageDirective extends AsyncHandler {
     @Input() public source: string;
